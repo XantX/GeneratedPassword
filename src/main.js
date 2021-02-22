8fe21aaf5cb1dfd564618b9ad8141e83f3174e0e
XantX <hp_xantx@hotmail.com>
2021-02-22 17:51:08 -0500

    Se hizo la interfaz Se agrego un icono y la interfaz de le generador

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 const url = require("url");
 const path = require("path");
+
+if (process.env.NODE_ENV !== "production") {
+  require("electron-reload")(__dirname, {});
+}
+
 let mainWindow;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
