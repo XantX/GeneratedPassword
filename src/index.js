ee8b05afcec051b540786a85b638e8e0f48a318e
XantX <hp_xantx@hotmail.com>
2021-02-23 23:06:16 -0500

    Se modifico la funcion de password Se concateni la contraseña generada

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const random = (Large) => {
+  let password = ""
+  // Se tiene que verificar que caracteres no se quieren en el password
   for (var i = 0; i < Large; i++) {
     const randomNumber = Math.floor(Math.random() * (126 - 33) + 33);
-    const valor = String.fromCharCode(randomNumber)
+    const valor = String.fromCharCode(randomNumber);
+    password = password + valor;
     console.log(valor, randomNumber);
   }
+  console.log(password);
 };
 random(8);
-
-
