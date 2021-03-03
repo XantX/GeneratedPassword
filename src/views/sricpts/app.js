1b537f93a9382f0cef2f6e8c5fb97d2cef53d485
XantX <hp_xantx@hotmail.com>
2021-03-03 14:02:44 -0500

    Se valido los caracteres Se implemento boton de copiado

--- src/views/sricpts/app.js
+++ src/views/sricpts/app.js
@@ -1,11 +1,10 @@
-const random = (Large) => {
+const random = (Large = 8) => {
   let completePassword = "";
   // Se tiene que verificar que caracteres no se quieren en el password
   for (var i = 0; i < Large; i++) {
-    const randomNumber = Math.floor(Math.random() * (126 - 33) + 33);
+    const randomNumber = Math.floor(Math.random() * (126 - 32) + 32);
     const charValueOfRandomNumber = String.fromCharCode(randomNumber);
     completePassword = completePassword + charValueOfRandomNumber;
-    console.log(charValueOfRandomNumber, randomNumber);
   }
   return completePassword;
 };
@@ -13,20 +12,46 @@ const random = (Large) => {
 const label = document.getElementById("NumberCharacters");
 label.addEventListener("keypress", (event) => {
   event.preventDefault();
-  let codeKey = event.keyCode;
+  const codeKey = event.keyCode;
   let valorKey = String.fromCharCode(codeKey);
   let KeyPressed = parseInt(valorKey);
-  if (KeyPressed) {
+  console.log(KeyPressed);
+  if (KeyPressed >= 0) {
     label.value += KeyPressed;
   }
 });
+
 // Se tiene que validar los caracteres
 function generated() {
   const numberOfCharacters = document.getElementById("NumberCharacters").value;
   const Character = parseInt(numberOfCharacters);
+  const passwordResultID = document.getElementById("password");
+
   if (Character) {
     const passwordComplete = random(Character);
-    const passwordResultID = document.getElementById("password");
+    passwordResultID.textContent = passwordComplete;
+  } else {
+    const passwordComplete = random();
     passwordResultID.textContent = passwordComplete;
   }
+}
+
+function myFunction() {
+  // Crea un campo de texto "oculto"
+  let aux = document.createElement("input");
+
+  // Asigna el contenido del elemento especificado al valor del campo
+  aux.setAttribute("value", document.getElementById("password").innerHTML);
+
+  // Añade el campo a la página
+  document.body.appendChild(aux);
+
+  // Selecciona el contenido del campo
+  aux.select();
+
+  // Copia el texto seleccionado
+  document.execCommand("copy");
+  alert("Copied the text: " + aux.value);
+  // Elimina el campo de la página
+  document.body.removeChild(aux);
 }
